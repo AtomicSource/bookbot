4f78ccfb527c37943cd4b2c5871c8efa695f85bf
AtomicSource <AtomicSource@users.noreply.github.com>
2025-08-28 00:48:02 +1000

    Added function to get number of words

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 
-def get_book_text(file_path):
+def get_book_text(file_path:str):
     book_contents = ""
     with open(file_path) as f:
         # read file
         book_contents = f.read()
     return book_contents 
 
+def get_word_number(contents:str):
+    words = contents.split()
+    number_of_words = len(words)
+    return number_of_words
+
 def main():
     file_path = "books/frankenstein.txt"
     book_contents = get_book_text(file_path)
-    print(book_contents)
+    number_of_words = get_word_number(book_contents)
+    print(number_of_words, "words found in the document")
 
 main()
