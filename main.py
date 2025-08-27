82dbeb4c161cc9cb930574429f64cfe658719813
AtomicSource <AtomicSource@users.noreply.github.com>
2025-08-28 00:32:40 +1000

    Wrote first function to get book contents

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+
+def get_book_text(file_path):
+    book_contents = ""
+    with open(file_path) as f:
+        # read file
+        book_contents = f.read()
+    return book_contents 
+
+def main():
+    file_path = "books/frankenstein.txt"
+    book_contents = get_book_text(file_path)
+    print(book_contents)
+
+main()
